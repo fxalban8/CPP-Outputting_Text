5bc8886f9c45d65e238105fd1afc09b1fe894149
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-29 21:21:25 -0500

    Learning to use FLUSH instead of ENDL

--- OutputtingText.cpp
+++ OutputtingText.cpp
@@ -2,7 +2,7 @@
 using namespace std; //std stands for standard
 
 int main(){
-
+	cout<<"Starting the program"<<flush; //flush only flushes the buffer
 	cout<<"Printing text"<<endl;	 /*ENDL adds and endline in the printing before 
 					 flushing the buffer*/
 	return 0;
