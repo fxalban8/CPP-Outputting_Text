30fc238617b679494994dac3921fcd01f93a6e34
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-29 21:13:47 -0500

    We've just created a cpp-file to print some text

---
+++ OutputtingText.cpp
@@ -0,0 +1,12 @@
+#include<iostream> //iostream stands for input-output stream
+using namespace std; //std stands for standard
+
+int main(){
+
+	cout<<"Printing text"<<endl	 /*ENDL adds and endline in the printing before 
+					 flushing the buffer*/
+	return 0;
+}
+
+
+
